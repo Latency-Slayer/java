90f82839d4ad88b167804d79df86a169c0aa5670
Erick <erick.miyagi@sptech.school>
2025-04-23 22:14:44 -0300

    Adicionando função mapHeader e entender melhor o mapComponent

--- src/main/java/ETL/Mapper.java
+++ src/main/java/ETL/Mapper.java
@@ -1,14 +1,34 @@
 package ETL;
 
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
+
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Mapper {
     public List<String> mapHeader(InputStream inputStream) throws IOException {
-
+        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8));
+        CSVParser parser = CSVFormat.DEFAULT.parse(reader); //leitura do arquivo CSV
+        return new ArrayList<>(parser.getHeaderMap().keySet()); //Pegar o header do CSV
     }
     public List<List<Double>> mapComponentData(InputStream inputStream) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8));
+        CSVParser parser = CSVFormat.DEFAULT.parse(reader);
 
+        List<List<Double>> listaDados = new ArrayList<>();
+
+        //CSVRecord representa um linha no meu CSV
+        for (CSVRecord row : parser){
+
+        }
+
+        return listaDados;
     }
 }
